13fb3b445477e7f07193e74c66da0e956fabc008
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-09-10 21:40:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void main()
+{
+    bool IsRain = false;
+    if (IsRain) {
+        cout << "PORNO" << endl;
+    }
+    else {
+        cout << 2 << endl;
+    }
+    
+}
